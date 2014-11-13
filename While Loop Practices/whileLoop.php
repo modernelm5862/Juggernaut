5d199ef6afd394ad6f9ec6bf18e498632c3cbad8
alfied <alfie.driver@cspencerltd.co.uk>
2014-11-13 13:13:49 +0000

    This is some practices that I have had with some While Loops.

---
+++ While Loop Practices/whileLoop.php
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="utf-8"?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>My first PHP page</title>
+		<link rel="stylesheet" href="styles.css" />
+	</head>
+
+	<body>
+		<div>
+		<h1>WHILE loop</h1>
+		<p>A while loop will look at an expression and if it evaulates to true then the statement will be executed and then the expression is then re-evaluated. If it is true then it will be looped again until the expression evauluates to false.</p>
+		<?php
+		echo ("Hello");
+
+		$
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		?>
+		</div>
+
+		<div id="ID">
+			<p>Alfie Driver</p>
+		</div> <!-- END OF ID DIV -->
+	</body>
+</html>
