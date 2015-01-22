4993216f3ee7f586557fc4bd2e0a8dff11797a76
alfied <alfie.driver@cspencerltd.co.uk>
2015-01-22 16:53:59 +0000

    Got this file working really well now. It writes to a csv files and creates a new line so that it is easier to read and see outlined blocks.

--- Reading and Writing to CSV file/writingtoCSV.php
+++ Reading and Writing to CSV file/writingtoCSV.php
@@ -1,18 +1,41 @@
 <?php
 
-$users = array(
-    array('Alfie', 'Driver', '20'),
-    array('James', 'Chadband', '30'),
-    array('Peter', 'Welch', '30')
-);
-
-$filename = "ignition.csv";
-
-try {
-    $fileWriter = fopen($filename, 'a');
-    foreach ($users as $user) {
-        fputcsv($fileWriter, $user);
-    }
-} catch (Exception $errorException) {
-    echo 'Caught exception: ', $errorException->getMessage();
+function fileLocation(){
+    $fileName = "file.csv";
+    return $fileName;
 }
+
+function fileSearcher($file)
+{
+    if (file_exists($file)){
+        echo 'The file ' . $file . ' exists' . PHP_EOL;
+    } else {
+        echo'The file ' . $file . ' does not exist but will be written' . PHP_EOL;
+    }
+}
+
+fileSearcher(fileLocation());
+
+function users()
+{
+    $users = array(
+        array('Alfie', 'Driver', '20'),
+        array('James', 'Chadband', '30'),
+        array('Peter', 'Welch', '30')
+    );
+    return $users;
+}
+
+function writeToFile($file, $users)
+{
+    $fileWriter = fopen($file, 'a');
+    foreach ($users as $inputtingUser) {
+        fputcsv($fileWriter, $inputtingUser, ',', '"');
+    }
+    echo 'File written Successfully' . PHP_EOL;
+    fclose($fileWriter);
+
+    file_put_contents($file, PHP_EOL, FILE_APPEND);
+}
+
+writeToFile(fileLocation(), users());
