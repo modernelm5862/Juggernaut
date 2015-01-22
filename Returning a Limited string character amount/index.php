d44da150ddc8fdee78aa7d911a7448ab077a5db8
alfied <alfie.driver@cspencerltd.co.uk>
2015-01-22 12:12:55 +0000

    Changing the lay out a little. No code has changed so far.

--- Returning a Limited string character amount/index.php
+++ Returning a Limited string character amount/index.php
@@ -2,42 +2,44 @@
 $message = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 $limit = 105;
 
-
-function detectLength($message)
+function detectLength($messageDetect)
 {
-    $messageLength = strlen($message);
+    $messageLength = strlen($messageDetect);
     return $messageLength;
 }
 
-function cuttingToLength($message, $limit)
+function cuttingToLength($messageCutter, $limitCutter)
 {
-    $messageLength = detectLength($message);
+    $messageLength = detectLength($messageCutter);
 //    echo $messageLength;
 //    echo PHP_EOL;
 
-    if ($messageLength > $limit){
-        $cutMessage = substr($message, 0, $limit);
+    if ($messageLength > $limitCutter){
+        $cutMessage = substr($messageCutter, 0, $limitCutter);
         return $cutMessage;
 
     } else {
-        return $message;
+        return $messageCutter;
     }
 }
 
-function detectingEndOfString ($message, $limit){
-    $cutMessage = cuttingToLength($message, $limit);
+function detectingEndOfString ($endOfMessage, $limitEnd)
+{
+    $cutMessage = cuttingToLength($endOfMessage, $limitEnd);
 
     $spaceFinder = strrpos($cutMessage, ' ');
     return $spaceFinder;
 }
 
-function correctingWord ($message, $limit){
-    $detectingEndOfString = detectingEndOfString($message, $limit);
+function correctingWord ($messageCorrect, $limit)
+{
+    $detectingEndOfString = detectingEndOfString($messageCorrect, $limit);
 
-    $findEndOfString = substr($message, 0, $detectingEndOfString);
+    $findEndOfString = substr($messageCorrect, 0, $detectingEndOfString);
     $finalMessage = $findEndOfString . '...';
 
     echo $finalMessage;
+    echo PHP_EOL;
 }
 
 correctingWord($message, $limit);
